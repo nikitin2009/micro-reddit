eefc1f3bf812c57d227ab81a393dd1b1f1d86cf9
Alex <nikitin2009@gmail.com>
2019-03-13 18:05:10 +0200

    Add validation for :username of User model

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_03_13_150633) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
