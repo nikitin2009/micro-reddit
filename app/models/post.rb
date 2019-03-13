088cb4ad5ea4d15645531ebc0a689d9a98bfbac7
Alex <nikitin2009@gmail.com>
2019-03-13 18:17:38 +0200

    Add Post model

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50}, uniqueness: true
+  validates :text, presence: true
+
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_150633) do
+ActiveRecord::Schema.define(version: 2019_03_13_160524) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
