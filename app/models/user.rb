e6f6e82c11e9de50b49ef83deb52161e05aba89e
Alex <nikitin2009@gmail.com>
2019-03-14 12:38:05 +0200

    Add Comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 50}, uniqueness: true
 
   has_many :posts
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_161747) do
+ActiveRecord::Schema.define(version: 2019_03_14_101756) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
 # Could not dump table "posts" because of following StandardError
 #   Unknown type 'reference' for column 'user_id'
